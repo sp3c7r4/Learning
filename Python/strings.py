343aecc29c2df1b9ed6aca61dcb4fb8d9592c445
sp3c7r4 <sp3c7r40x00@gmail.com>
2025-09-14 16:34:46 +0100

    feat: working with strings

---
+++ Python/strings.py
@@ -0,0 +1,12 @@
+# Working with Strings
+
+course = "Python Programming"
+
+#Length
+print(len(course)) #18
+# Accessing specific characters in a string
+print( course[0], course[-1] ) #Pg
+#Slicing strings
+print ( course[0:3], course[0:] ) #pyt Python Programming
+print( course[:3] ) #Pyt
+print( course[:] ) #Python Programming
